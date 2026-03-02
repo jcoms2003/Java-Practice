c05243a45494328dd4555b5c145d5454ae46ba1c
jcoms2003 <jcomsusmc2022@gmail.com>
2026-03-02 13:25:47 -0700

    adding inductive types

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
 	public static void main(String[] args) {
+		
+	}
+	// List inductive types
+	// Every record must have the <TYPE> brfore the ()
+	sealed interface MyList<B> permits Nil, cons{}
+		record Nil<B>() implements MyList<B> {}
+		record cons<B>(B b, MyList<B> tail) implements MyList<B>{}
+	// Tree inductive types
+	sealed interface Tree<A> permits Leaf, Branch {}
+		record Leaf<A>() implements Tree<A>{}
+		record Branch<A>(A a, Tree<A> left, Tree<A> right) implements Tree<A> {}
+
+	
+/*	public static void main(String[] args) {
 		System.out.println("Hello, World");
 		System.out.println("From, Java!");
 	}
@@ -20,4 +34,15 @@ public class Main {
 		System.out.println(one);
 		System.out.println(two);
 	}
+	int x = 4;
+	int y = 3;
+	if (y == x) {
+		System.out.println("Hello");
+		}
+	else (x > y) {
+		System.out.println("WOW");
+	}
+	*/
 }
+
+
