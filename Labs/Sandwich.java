a955b001caacfdcb4544a4ed02641aa1a064870f
jcoms2003 <jcomsusmc2022@gmail.com>
2026-03-30 12:14:55 -0600

    changing types

--- Labs/Sandwich.java
+++ Labs/Sandwich.java
@@ -1,19 +1,26 @@
+// Imports
+import java.util.Scanner; // for reading user input
+import java.util.ArrayList; //for using Array List (eg. lists)
+import java.util.Arrays;
+
+
 public class Sandwich {
     // Class Variables
     public static final boolean IS_GOOD = true;
     private String bread;
-    private String meat;
+    private String[] meat;
     private String sauce;
     private String topping;
     private int size;
 
     // Sandwich constuctor
-    public Sandwich(String bread, String meat, String sauce, String topping, int size) {
+    public Sandwich(String bread, String[] meat, String sauce, String topping, int size) {
         this.bread = bread;
-        this.meat = meat;
+        this.meat = new String[meat.length];
         this.sauce = sauce;
         this.topping = topping;
         this.size = size;
+
     }
 
     /* Setters */
@@ -23,8 +30,8 @@ public class Sandwich {
         bread = str;
     }
     // Setter for meat
-    public void setMeat(String str) {
-        meat = str;
+    public void setMeat(String[] lst) {
+        meat = lst;
     }
     // Setter for sauce
     public void setSauce(String str) {
@@ -46,7 +53,7 @@ public class Sandwich {
         return bread;
     }
     // Getter for meat
-    public String getMeat() {
+    public String[] getMeat() {
         return meat;
     }
     // Getter for sauce
@@ -61,6 +68,8 @@ public class Sandwich {
     public int getSize() {
         return size;
     }
+
+    // Switch for size to string for set sizes
     public String sizeToString() {
         switch (size){
             case 3:
@@ -74,10 +83,15 @@ public class Sandwich {
         }
     }
 
+    // Testing
+    public static String arrayToString(String[] str) {
+        return Arrays.toString(str);
+    }
+
     // Overide toString
     @Override
     public String toString() {
-        return "Sandwich[bread=" + bread + ", meat=" + meat + ", sauce=" + sauce +
+        return "Sandwich[bread=" + bread + ", meat=" + Arrays.toString(meat) + ", sauce=" + sauce +
             ", topping=" + topping + ", size=" + size + "]";
 }
 
@@ -85,12 +99,15 @@ public class Sandwich {
     // public static main method
     public static void main(String [] args) {
         System.out.println("Sandwich Class");
-        Sandwich sandwich1 = new Sandwich("White", "Ham", "Mustard", "Lettuce", 12);
+        Sandwich sandwich1 = new Sandwich("White", new String[] {"Ham", "Beef"}, "Mustard", "Lettuce", 12);
         System.out.println(sandwich1);
         sandwich1.setBread("Wheat");
         System.out.println(sandwich1);
         System.out.println(sandwich1.getBread());
         System.out.println(sandwich1.getSauce());
         System.out.println(sandwich1.sizeToString());
+        sandwich1.setMeat(new String[] {"ham", "beef"});
+        System.out.println(sandwich1);
+        System.out.println(arrayToString(sandwich1.getMeat()));
     }
 }
