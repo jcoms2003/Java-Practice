86a13fdb0c1c7664c6a27aa31c9bb47bc8915f80
jcoms2003 <jcomsusmc2022@gmail.com>
2026-04-06 18:55:04 -0600

    final commit lab4

--- Labs/lab4/Cat.java
+++ Labs/lab4/Cat.java
@@ -1,10 +1,18 @@
 public class Cat extends Pets {
-    public Cat(String name, String breed){
+    private String badName;
+    public Cat(String name, String breed, String badName){
       super(name, breed);
+      this.badName = badName;
     }
     @Override
     public String makeNoise() {
       return "meow";
+    }
+    public void setBadName(String s) {
+      badName = s;
+    }
+    public String getName() {
+      return name + " aka " + badName;
     }
     
     public static void main(String[] args){
