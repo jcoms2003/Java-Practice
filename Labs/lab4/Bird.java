f01fabce67544d1b1286292d6b899cadafe343a2
jcoms2003 <jcomsusmc2022@gmail.com>
2026-04-16 11:59:12 -0600

    expanding lab4

--- Labs/lab4/Bird.java
+++ Labs/lab4/Bird.java
@@ -3,19 +3,40 @@ public class Bird extends Pets {
         super(name, breed);
     }
 
-    //TODO put your override for makeNoise here
+    @Override
+    public String makeNoise() {
+        return "chirp";
+    }
 
-    //TODO put your overload for makeNoise here
+    public String makeNoise(int i) {
+        if(i % 4 == 0) {
+            return "squawk";
+        } else if(i % 3 == 0) {
+            return "tweet";
+        } else if( i % 2 == 0) {
+            return "chirp";
+        } else { return "caw caw"; }
+    }
 
     //this method returns "[name] ate some seeds", where [name] is the Class variable name
     public String eatSeeds(){
-        return getName() + "ate some seeds";
+        return getName() + " ate some seeds";
     }
 
-    //TODO put your overloads for eatSeeds() here
+    public String eatSeeds(boolean b) {
+        if(b) {
+            return getName() + " ate some seeds";
+        } else { return getName() + " did not eat any seeds"; }
+    }
+
+    public String eatSeeds(int i) {
+        return getName() + " ate " + i + " seeds";
+    }
     
     
     public static void main(String[] args){
-
+        Bird Feathers = new Bird("Feathers", "parrot");
+        System.out.println(Feathers.eatSeeds(false)); //this should return "Feathers did not eat any seeds"
+        System.out.println(Feathers.eatSeeds(17)); //this should return "Feathers ate 17 seeds"
     }
 }
--- Labs/lab4/Cat.java
+++ Labs/lab4/Cat.java
@@ -8,14 +8,21 @@ public class Cat extends Pets {
     public String makeNoise() {
       return "meow";
     }
+
     public void setBadName(String s) {
       badName = s;
     }
     public String getName() {
       return name + " aka " + badName;
     }
+
+    public String getName(String s) {
+      return name + " aka " + s;
+    }
     
     public static void main(String[] args){
-
+      Cat Gyorik = new Cat("Gyorik", "russian blue", "Wretched Boy");
+      System.out.println(Gyorik.getName());   // this should return "Gyorik aka Wretched Boy"
+      System.out.println(Gyorik.getName("Mr. Man")); // this should return "Gyorik aka Mr. Man"
     }
 }
