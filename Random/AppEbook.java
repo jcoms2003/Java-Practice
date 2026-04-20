05dba90075e37efa6b50af8dd51c91f45e6e020d
jcoms2003 <jcomsusmc2022@gmail.com>
2026-04-20 12:17:05 -0600

    class sheet

---
+++ Random/AppEbook.java
@@ -0,0 +1,8 @@
+public class AppEbook {
+	// EBook object
+	/**********NOTE:This will not compile, just practice.**********/
+	public static void main(String [] args) {
+		Ebook book1 = new Ebook("Book1", ["Page1", "Page2"]);
+		System.out.println(book1.read());
+	}
+}
