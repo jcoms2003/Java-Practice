cbf1e04a455909645d1a122a3e5a7798a74ecb56
jcoms2003 <jcomsusmc2022@gmail.com>
2026-04-20 12:43:43 -0600

    factorial

--- Random/Fact.java
+++ Random/Fact.java
@@ -6,6 +6,11 @@ public class Fact {
 			return 1;
 		} return f * fact(f-1);
 	}
+	public static String reverse(String s,s.length()-1 {
+		String reversed = "";
+		if(index < 0) {return "";}
+		return s.charAt(index) + reverse(s,index - 1);
+	}
 
 	public static void main(String[] args) {
 		System.out.println(fact(5));
